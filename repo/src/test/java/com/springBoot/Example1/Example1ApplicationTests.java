6d3d5cc2c24fa0a4acc6846c0e6c5afd9f2eba9a
Vishal <vishalkhairagarh@gmail.com>
2023-12-11 21:40:22 +0530

    final commit

---
+++ repo/src/test/java/com/springBoot/Example1/Example1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.Example1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Example1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
